a40ddb0a78712f41b316ccad116e7e6a9c279fb6
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-04 10:35:48 +0100

    adding the form event listener

---
+++ js/UI.js
@@ -0,0 +1,7 @@
+class UI {
+
+    // Display message to user
+    displayMessage(){
+        console.log('error from display method')
+    }
+}
---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Instantiate all classes
+const ui = new UI();
+
+
+// Create event listeners
+function eventListeners() {
+    // Add event listener when form is submitted
+    const searchForm = document.querySelector('#search-form');
+    if(searchForm) {
+        searchForm.addEventListener('submit', getCocktails);
+    }
+}
+
+eventListeners();
+
+// Get Cocktails
+function getCocktails(e) {
+    e.preventDefault();
+
+    const searchTerm = document.querySelector('#search').value;
+
+    // Check if searchTerm is not empty
+    if(searchTerm.trim() !== '') {
+        console.log('query rest api');
+    } else {
+        // Display message to user
+        ui.displayMessage();
+    }
+    
+}
